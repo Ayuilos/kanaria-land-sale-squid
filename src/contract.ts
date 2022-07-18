04ad61ab6521118d0615cd71bd91b03c867b00f9
Ayuilos <ekklovepeace@gmail.com>
2022-07-18 12:46:20 +0800

    fix: change to private RPC but use HTTPS request instead

--- src/contract.ts
+++ src/contract.ts
@@ -4,6 +4,7 @@ import * as kanariaNew from "./abi/kanariaNew";
 import * as rmrk from "./abi/rmrk";
 
 export const CHAIN_NODE = "wss://wss.api.moonriver.moonbeam.network";
+export const HTTPS_CHAIN_NODE = "https://moonriver.blastapi.io/85537306-e2b1-48a5-8842-58940c31b184";
 
 // work well locally but not work well on subsquid cloud, so just leave it here for future use
 export const FOR_FREQUENT_RPC_INVOKE_NODE = "wss://moonriver.blastapi.io/85537306-e2b1-48a5-8842-58940c31b184";
@@ -27,10 +28,12 @@ export const contractKanariaNew = new ethers.Contract(
   new ethers.providers.WebSocketProvider(CHAIN_NODE)
 );
 
+const httpsProvider = new ethers.providers.JsonRpcProvider(HTTPS_CHAIN_NODE);
+
 export const contractKanariaNewForRPC = new ethers.Contract(
   newLandSaleContractAddr.toLowerCase(),
   kanariaNew.abi,
-  new ethers.providers.WebSocketProvider(CHAIN_NODE)
+  httpsProvider
 );
 
 export const contractRMRK = new ethers.Contract(
